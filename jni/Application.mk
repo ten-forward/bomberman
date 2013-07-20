2249fddd03af50da1dbdcb6e415600d41112c493
David Siaw <davidsiaw@gmail.com>
2013-07-20 13:50:53 +1200

    use clang to build - better error messages

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,3 +2,4 @@
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
+NDK_TOOLCHAIN_VERSION=clang3.2
