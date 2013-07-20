6fd54a43d7d1c38d79d803f477f5606571fd6c79
David Siaw <davidsiaw@gmail.com>
2013-07-20 16:59:35 +1200

    change project to bomberman

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
-
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
-NDK_TOOLCHAIN_VERSION=clang3.2
+NDK_TOOLCHAIN_VERSION := clang3.2
+APP_ABI := armeabi
