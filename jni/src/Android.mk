5a6d4b96edb507d292cae71d3a166a2ede0f862a
Sébastien Bigot <sb@unhexhexium.net>
2013-09-20 01:05:54 +1200

    Droid mkfile fxup

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -19,8 +19,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_ttf_PATH)
 
 # Add your application source files here...
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
-	$(shell find . -name "*.cpp")
+LOCAL_SRC_FILES := $(shell cd jni/src && find . -name "*.cpp") \
+	$(SDL_PATH)/src/main/android/SDL_android_main.cpp \
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf
 
