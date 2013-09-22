e090b0811ba9d0437f9dd0f71fe25dfdb0867a5c
David Siaw <davidsiaw@gmail.com>
2013-09-22 18:09:39 +1200

    tidy up some more and move android stuff into android only

--- provision.sh
+++ provision.sh
@@ -13,4 +13,10 @@ then
     wget --timeout=30 http://dl.google.com/android/ndk/android-ndk-r9-linux-x86.tar.bz2 -O ndk.tar.bz2
     tar -xf ndk.tar.bz2
     export ANDROID_HOME=$(pwd)/android-sdk-linux 
+    wget -q http://dl.google.com/android/android-sdk_r22.0.1-linux.tgz
+    tar xzvf android-sdk_r22.0.1-linux.tgz > /dev/null
+    $ANDROID_HOME/tools/android list sdk
+    echo "y" | $ANDROID_HOME/tools/android -s update sdk --filter 13,1,2,3 --no-ui
+    $ANDROID_HOME/tools/android list targets
+    $ANDROID_HOME/tools/android -s update project --path . --target "android-10"
 fi
