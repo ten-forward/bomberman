8759cab872218deb4f5dbba82fbb66208fb4e906
David Siaw <davidsiaw@gmail.com>
2013-09-23 09:15:49 +1200

    wireframe for scripting support

--- jni/src/computer.cpp
+++ jni/src/computer.cpp
@@ -3,37 +3,47 @@
 namespace bomberman {
 namespace bestiary {
 	ComputerPtr Computer::Create(const std::string &iName, const std::string &iSpriteName, int iInputStateIdx, SDL_Renderer* iRenderer, bool* alive)
-
 	{
+		SDL_RWops *rw = SDL_RWFromFile("aiscripts/example.lua", "r");
 
-		auto player = std::make_shared<Computer>();
+		auto size = rw->size(rw);
+		char* script = new char[size];
+		rw->read(rw, script, sizeof(char), size);
 
+		Lua lua;
+		auto coroutine = lua.CreateCoroutine();
+
+		SDL_FreeRW(rw);
+
+		auto player = std::make_shared<Computer>(coroutine);
 		player->_name = iName;
-
 		player->_spriteName = iSpriteName;
-
 		player->zlevel = 2;
-
 		player->_frameId = 3;
-
 		player->_nextFrameDueTime = 0;
-
 		player->_nextUpdateDueTime = 0;
-
 		player->_state = IdleDown;
-
 		player->_inputStateIdx = iInputStateIdx;
-
 		player->InitializeGraphicRessources(iRenderer);
-
 		player->_alive = alive;
 
-		return player;
+		coroutine.RunScript(script);
+		delete[] script;
 
+		return player;
 	}
 
-	//void Computer::Evolve(const std::vector<InputState>& /*iInputs*/, uint32_t /*iTimestamp*/, const MapConstPtr &/*iPresentMap*/, const MapPtr &/*iFutureMap*/) const
-	//{
-	//}
+	Computer::Computer(LuaCoroutine coroutine) : _coroutine(coroutine)
+	{ }
+
+	void Computer::Evolve(const std::vector<InputState>& iInputs, uint32_t iTimestamp, const MapConstPtr &iPresentMap, const MapPtr &iFutureMap) const
+	{
+		Player::Evolve(iInputs, iTimestamp, iPresentMap, iFutureMap);
+	}
+
+	void Computer::Interact(const std::vector<InputState>& iInputs, uint32_t iTimestamp, const EntitySet &iEntities)
+	{
+		Player::Interact(iInputs, iTimestamp, iEntities);
+	}
 }
 }
--- jni/src/computer.hpp
+++ jni/src/computer.hpp
@@ -2,7 +2,8 @@
 
 #include "player.hpp"
 
-// STL - libstdc++
+#include <luacppinterface.h>
+
 #include <memory>
 
 namespace bomberman {
@@ -12,10 +13,14 @@ namespace bestiary {
 	typedef std::shared_ptr<Computer> ComputerPtr;
 
 	class Computer : public Player {
+		
+		LuaCoroutine _coroutine;
+
 	public:
 		static ComputerPtr Create(const std::string &iName, const std::string &iSpriteName, int iInputStateIdx, SDL_Renderer* iRenderer, bool* alive);
-		//void Evolve(const std::vector<InputState>& iInputs, uint32_t iTimestamp, const MapConstPtr &iPresentMap, const MapPtr &iFutureMap) const;
-		//virtual void Interact(const std::vector<InputState>& , uint32_t , const EntitySet &) {}
+		Computer(LuaCoroutine coroutine);
+		virtual void Evolve(const std::vector<InputState>& iInputs, uint32_t iTimestamp, const MapConstPtr &iPresentMap, const MapPtr &iFutureMap) const;
+		virtual void Interact(const std::vector<InputState>& , uint32_t , const EntitySet &);
 	};
 }
 }
