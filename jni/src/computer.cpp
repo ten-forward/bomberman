3932d1fb3c176f915190312ffbb93690249ba76d
David Siaw <davidsiaw@gmail.com>
2013-09-22 21:47:09 +1200

    fix computer player for now

--- jni/src/computer.cpp
+++ jni/src/computer.cpp
@@ -3,23 +3,37 @@
 namespace bomberman {
 namespace bestiary {
 	ComputerPtr Computer::Create(const std::string &iName, const std::string &iSpriteName, int iInputStateIdx, SDL_Renderer* iRenderer, bool* alive)
+
 	{
+
 		auto player = std::make_shared<Computer>();
+
 		player->_name = iName;
+
 		player->_spriteName = iSpriteName;
+
 		player->zlevel = 2;
+
 		player->_frameId = 3;
+
 		player->_nextFrameDueTime = 0;
+
 		player->_nextUpdateDueTime = 0;
+
 		player->_state = IdleDown;
+
 		player->_inputStateIdx = iInputStateIdx;
+
 		player->InitializeGraphicRessources(iRenderer);
+
 		player->_alive = alive;
+
 		return player;
+
 	}
 
-	void Computer::Evolve(const std::vector<InputState>& /*iInputs*/, uint32_t /*iTimestamp*/, const MapConstPtr &/*iPresentMap*/, const MapPtr &/*iFutureMap*/) const
-	{
-	}
+	//void Computer::Evolve(const std::vector<InputState>& /*iInputs*/, uint32_t /*iTimestamp*/, const MapConstPtr &/*iPresentMap*/, const MapPtr &/*iFutureMap*/) const
+	//{
+	//}
 }
 }
