f43aaa8a3ce19e2e489c2f4a27e5bc6ca5fc2609
David Siaw <davidsiaw@gmail.com>
2013-09-22 18:43:35 +1200

    export env variable

--- provision.sh
+++ provision.sh
@@ -12,7 +12,6 @@ if [ $THE_MAKEFILE == "Makefile.android" ]
 then 
     wget --timeout=30 http://dl.google.com/android/ndk/android-ndk-r9-linux-x86.tar.bz2 -O ndk.tar.bz2
     tar -xf ndk.tar.bz2
-    export ANDROID_HOME=$(pwd)/android-sdk-linux 
     wget -q http://dl.google.com/android/android-sdk_r22.0.1-linux.tgz
     tar xzvf android-sdk_r22.0.1-linux.tgz > /dev/null
     $ANDROID_HOME/tools/android list sdk
